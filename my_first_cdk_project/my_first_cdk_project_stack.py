5849b4acbbc06a8193fc93f9f4f1ad04898d52da
JV <johnvandervliet@protonmail.com>
2021-04-27 16:07:17 -0600

    finished with lesson one

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
-    aws_s3 as _s3,  #_s3 is being used to avoid collitions with other packages   
+    aws_s3 as _s3,  #_s3 is being used to avoid collitions with other packages
+    aws_iam as _iam,   
     core
 )
 
@@ -13,7 +14,28 @@ class MyFirstCdkProjectStack(core.Stack):
         _s3.Bucket(
             self, 
             "myBucketId",
-            bucket_name="jv-myfirstcdkproject-456-dev",
+            bucket_name="jv-myfirstcdkproject-456-dev-1",
             versioned=True,
-            encryption=_s3.BucketEncryption.KMS_MANAGED
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketId1"
+        )
+
+        #print(mybucket.bucket_name)
+
+        _iam.Group(
+            self,
+            "gid"
+        )
+
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name,
+            description=f"My first CDK bucket",
+            export_name="myBucketOutput1"
         )
